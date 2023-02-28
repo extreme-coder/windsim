9d48c6b64c942cd17f4125819461f9bbd521a179
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-02-28 12:07:20 -0800

    turbine dropping animation

--- src/Marker.jsx
+++ src/Marker.jsx
@@ -17,7 +17,7 @@ const Marker = ({ options, position, map, id, isSelected, hasData, onDrag, selec
         setMarker(new window.google.maps.Marker({
           map: map,
           //draggable: true,
-          //animation: google.maps.Animation.DROP
+          animation: window.google.maps.Animation.DROP
         }));
     } 
     if (marker) {
